8a0263f9c8e7e3b4f5fa617cef9492236ac9b697
abdallah <abdallajbour@outlook.com>
2024-06-05 23:17:15 +0300

    Function

---
+++ a.js
@@ -0,0 +1,68 @@
+//Q1//////////////////////////////////////////////////
+
+// console.log(rev_num);
+// let arr = [1, 2, 3, 4, 5];
+// let x = arr.reverse();
+// let y = "";
+// function rev() {
+//   for (let i = 0; i < arr.length; i++) {
+//     y += arr[i];
+//   }
+//   alert(y);
+// }
+// let x = 123456;
+// let y = x.toString();
+// let a = "";
+// function rev() {
+//   for (let i = 5; i >= 0; i--) {
+//     a += y[i];
+//   }
+//   alert(a);
+// }
+// rev();
+//Q2///////////////////////////////////////////////////////////////
+// function num() {
+//   for (let i = 0; i <= 20; i++) {
+//     if (i % 2 == 0) {
+//       console.log("The Number Is Even :", i);
+//     } else {
+//       console.log("The Number Is Odd :", i);
+//     }
+//   }
+// }
+// num();
+//Q3///////////////////////////////////////////////////////////////
+// function sor() {
+//   let x = "orange";
+//   let y = x.split("");
+//   let s = y.sort();
+//   console.log(s);
+// }
+// sor();
+//Q4///////////////////////////////////////////////////////////////
+// let x = 5362478;
+// y = x.toString();
+// s = y.split("");
+// let arr = [];
+// for (let i = 0; i < 6; i++) {
+//   arr.push(parseInt(s[i]));
+// }
+// console.log(arr);
+// for (let i = 0; i < 6; i++) {
+//   let h = arr[i];
+//   if (arr[i] % 2 == 0) {
+//     console.log(arr.join("-"));
+//   }
+// }
+// for (let i = 0; i <= 6; i++) {
+//   if (s[i] % 2 == 0) {
+//     let m = s.join("-");
+//     console.log(m);
+//   }
+// }
+//Q5///////////////////////////////////////////////////////////////
+// let a = prompt("Enter Your Age");
+// function age() {
+//   a >= 18 ? alert("The user is Adult") : alert("The user is Minor");
+// }
+// age();
